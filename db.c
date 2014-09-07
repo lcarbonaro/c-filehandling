3f79616a253bc0bc400bab109e0ba3fef0bd5e86
Les Carbonaro <lcarbonaro@gmail.com>
2014-09-07 17:54:05 +0000

    added some comments etc.

--- db.c
+++ db.c
@@ -1,41 +1,58 @@
 #include <stdio.h>
 #include <mysql.h>
 
-int main(int argc, char **argv)
+int main(void)
 {
-  printf("MySQL client version: %s\n", mysql_get_client_info());
-  
-   MYSQL *conn;
-   MYSQL_RES *res;
-   MYSQL_ROW row;
-   char *server = "0.0.0.0";   
-   char *user = "lcarbonaro";
-   char *password = ""; 
-   char *database = "test";
-   conn = mysql_init(NULL);
-   /* Connect to database */
-   if (!mysql_real_connect(conn, server,user, password, database, 0, NULL, 0)) {
-      fprintf(stderr, "%s\n", mysql_error(conn));
-      // exit
-   }
-   /* send SQL query */
-   
-   char *sql = "select * from test";    // was: show tables
-   
-   if (mysql_query(conn, sql)) {
-      fprintf(stderr, "%s\n", mysql_error(conn));
-      // exit
-   }
-   res = mysql_use_result(conn);
-   /* output table name */
-   printf("MySQL Tables in mysql database:\n");
-   while ((row = mysql_fetch_row(res)) != NULL)
-      printf("%s: %s \n", row[0],row[1]);
-   /* close connection */
-   mysql_free_result(res);
-   mysql_close(conn);
+    MYSQL *conn;
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+    
+    char *server = "0.0.0.0";
+    char *user = "lcarbonaro";
+    char *password = "";
+    char *database = "test";
 
-  return 0;
+    char *sql;
+
+    // just checking mysql library
+    //printf("MySQL client version: %s\n", mysql_get_client_info());
+
+    conn = mysql_init(NULL);
+
+    // connect to database
+    // assumes MySQL server is up and running, and that database exists
+    if (!mysql_real_connect(conn, server,user, password, database, 0, NULL, 0))
+    {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+    }
+    else
+    {
+        // run sql query
+        sql = "select * from `test`";    // was: show tables
+
+        if (mysql_query(conn, sql))   // run the sql
+        {
+            fprintf(stderr, "%s\n", mysql_error(conn));
+        }
+        else
+        {
+            printf("SQL statement:%s\n",sql);
+            res = mysql_use_result(conn);  // assign query result to variable res
+
+            // output query results
+            printf("SQL results:\n");
+            while ((row = mysql_fetch_row(res)) != NULL)
+            {
+                printf("%s: %s \n", row[0],row[1]);
+            }
+        }
+
+        // free up result variable and close connection
+        mysql_free_result(res);
+        mysql_close(conn);
+    }
+
+    return 0;
 }
 
 // ref:
